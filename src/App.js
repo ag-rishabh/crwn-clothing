1f610764043c836f9a4ef977a28112a73a3bb676
Rishabh <rishabh33agarwal@gmail.com>
2020-10-26 15:59:33 +0530

    add preview component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/Homepage';
+import ShopPage from './pages/shop/Shoppage';
 
-const HatsPage = () => <div>Hats Page.</div>
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/shop/Shoppage.jsx
+++ src/pages/shop/Shoppage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionPreview from '../../components/collection-preview/CollectionPreview';
 import SHOP_DATA from './shop.data';
 
 class ShopPage extends React.Component {
@@ -7,7 +8,14 @@ class ShopPage extends React.Component {
     }
 
     render() {
-        return (<div>SHop Page</div>);
+        const {collections} = this.state;
+        return (
+            <div className="shop-page">
+                {collections.map(({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))}
+            </div>
+        );
     }
 }
 
